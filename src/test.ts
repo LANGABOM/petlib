e4afa8a89179bbd6a62e3f675ecf806c143bb64f
Mitch Chanza <mitch.chanza@gmail.com>
2024-03-13 14:09:08 +0200

    🟢 src/test.ts 🔴 None 🛠️ None -> Added import statements and console logs

---
+++ src/test.ts
@@ -0,0 +1,5 @@
+import {add,sub} from "."
+import nqobile from "./modules/nqobile" // require('./modules/nqobile')
+console.log(add(5, 8)) // 13
+console.log(sub(5, 8)) // 5
+console.log(nqobile("nqobile"))
