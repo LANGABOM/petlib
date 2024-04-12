7179156c19523d6cc59f72e18a2f16d86fbff80f
Mitch Chanza <mitch.chanza@gmail.com>
2024-04-12 11:35:56 +0200

    chore: add IcartItens

--- ui-library/src/components/ShoppingCart/ShoppingCart.d.ts
+++ ui-library/src/components/ShoppingCart/ShoppingCart.d.ts
@@ -1,3 +1,11 @@
 export interface IShoppingCartProps {
     option: "ICON"|"BUTTON"
 }
+
+export interface ICartItems {
+    id: number
+    name: string
+    description: string
+    price: number
+    image: string
+}
