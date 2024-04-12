6b91f44cd48eb8fdb1a4a92524fd3a861d8c0d06
Mitch Chanza <mitch.chanza@gmail.com>
2024-04-12 12:32:27 +0200

    🛠️ ui-library/src/App.tsx -> Expanded imports, added items list

--- ui-library/src/App.tsx
+++ ui-library/src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
-import { ShoppingCart } from './components'
-function App() {
+import { ShoppingCart, ICartItems, ShoppingCard } from './components'
 
+
+function App() {
+  const itemsList: ICartItems[] = [
+    { id: 1, name: "n1", description: "The description", price: 40, image: "https://flowbite.com/docs/images/products/apple-watch.png" },
+    { id: 2, name: "The second item", description: "The description", price: 40, image: "https://flowbite.com/docs/images/products/apple-watch.png" },
+    { id: 3, name: "The shopping cart", description: "The description", price: 40, image: "https://flowbite.com/docs/images/products/apple-watch.png" },
+  ]
   return (
-    <>
-    <ShoppingCart option="ICON" />
-    <ShoppingCart option="BUTTON" />
-    </>
+    <div className='mx-auto space-y-6 max-w-7xl my-8'>
+      <ShoppingCart option="ICON" />
+      <div className="flex space-x-4 flex-wrap">
+        {
+          itemsList.map(item => <div key={item.id}>
+            <ShoppingCard item={item} />
+          </div>)
+        }
+      </div>
+
+    </div>
   )
 }
 
