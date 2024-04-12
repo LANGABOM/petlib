dbebb2e6d1c992b898a65ca81e6a871f705514e6
Mitch Chanza <mitch.chanza@gmail.com>
2024-04-12 11:36:48 +0200

    feat: implement addToCart function and clearCart function

--- ui-library/src/components/ShoppingCart/utils/hooks/useShoppingCart.ts
+++ ui-library/src/components/ShoppingCart/utils/hooks/useShoppingCart.ts
@@ -3,8 +3,14 @@ import { create } from 'zustand'
 import { IUseShoppingCartProps } from './useShoppingcart.d'
 export const useShoppingCart= create<IUseShoppingCartProps>((set)=>({
     cart:[],
-    addToCart:(item:any)=>({cart: [item]}),
+    addToCart:(item:any)=>set((state)=>({cart:[...state.cart,item]})),
+    clearCart:()=>set(({cart:[]})),
     removeFromCart:(item:any)=>({cart: [item]}), // remove a specific item in the array
-    clearCart:()=>set(),
     totalItems:{totalItems:0,totalAmount:0}
 }))
+
+// 
+const names = ["n1", "n2","n8",0,7]
+names.length=0
+console.log(names) // []
+// {id: 1, name: "n1",description:"", price:40,image:""}
