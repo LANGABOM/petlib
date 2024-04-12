7ad67eff55c05052e1a8cb79c9d7522d404aa1ad
Mitch Chanza <mitch.chanza@gmail.com>
2024-04-12 12:52:42 +0200

    🛠️ useShoppingCart.ts -> Added persistence to hook

--- ui-library/src/components/ShoppingCart/utils/hooks/useShoppingCart.ts
+++ ui-library/src/components/ShoppingCart/utils/hooks/useShoppingCart.ts
@@ -1,16 +1,19 @@
 import { create } from 'zustand'
 import { ICartItems } from '../../ShoppingCart.d'
 import { IUseShoppingCartProps } from './useShoppingcart.d'
-export const useShoppingCart= create<IUseShoppingCartProps>((set)=>({
-    cart:[],
-    addToCart:(item:ICartItems)=>set((state)=>({cart:[...state.cart,item]})),
-    clearCart:()=>set(({cart:[]})),
-    removeFromCart:(item:ICartItems)=>({cart: [item]}), // remove a specific item in the array
-    totalItems:{totalItems:0,totalAmount:0},
-}))
+import { persist } from 'zustand/middleware'
 
-// 
-const names = ["n1", "n2","n8",0,7]
-names.length=0
-console.log(names) // []
-// {id: 1, name: "n1",description:"", price:40,image:""}
+export const useShoppingCartHook = create(persist<IUseShoppingCartProps>((set) => ({
+    cart: [],
+    addToCart: (item: ICartItems) => set((state) => ({ cart: [...state.cart, item] })),
+    clearCart: () => set(({ cart: [] })),
+    removeFromCart: (item: ICartItems) => ({ cart: [item] }), // remove a specific item in the array
+    totalItems: { totalItems: 0, totalAmount: 0 },
+}
+),
+    {
+        name: "shoppingCart",
+        getStorage: () => sessionStorage
+    }
+)
+)
